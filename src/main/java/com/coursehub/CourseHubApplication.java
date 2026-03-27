64af9e36732c0e7c7ae60b213f43bc838ccf1bad
Nishant Yadav <ny926602@gmail.com>
2026-03-27 08:57:58 +0530

    chore: initialize spring boot maven project

---
+++ src/main/java/com/coursehub/CourseHubApplication.java
@@ -0,0 +1,12 @@
+package com.coursehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseHubApplication.class, args);
+    }
+}
