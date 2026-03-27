14472c7c7be0203b827e59cd80601ee0d8b3cb01
Nishant Yadav <ny926602@gmail.com>
2026-03-27 08:58:59 +0530

    feat: add course dto models

---
+++ src/main/java/com/coursehub/dto/CourseRequestDto.java
@@ -0,0 +1,29 @@
+package com.coursehub.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseRequestDto {
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 150, message = "Title must not exceed 150 characters")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 500, message = "Description must not exceed 500 characters")
+    private String description;
+
+    @NotBlank(message = "Instructor is required")
+    @Size(max = 120, message = "Instructor must not exceed 120 characters")
+    private String instructor;
+}
---
+++ src/main/java/com/coursehub/dto/CourseResponseDto.java
@@ -0,0 +1,20 @@
+package com.coursehub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseResponseDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String instructor;
+}
