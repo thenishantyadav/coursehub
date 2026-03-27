ad3f3abd09674692515833a566ffc020e37a9aa0
Nishant Yadav <ny926602@gmail.com>
2026-03-27 08:59:33 +0530

    test: add application context test configuration

---
+++ src/test/java/com/coursehub/CourseHubApplicationTests.java
@@ -0,0 +1,12 @@
+package com.coursehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
